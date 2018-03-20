6e77972b0634a786737e7ffee4d425699de2acf4
wertanwee <57070057@kmitl.ac.th>
2018-03-20 20:13:32 +0700

    add milestones

---
+++ src/components/CreateTimeline.vue
@@ -0,0 +1,39 @@
+<template lang="html">
+  <div class="timeline-item">
+    <div class="timeline-item-date">21 <small>DEC</small></div>
+    <div class="timeline-item-divider"></div>
+    <div class="timeline-item-content">
+      <div class="timeline-item-inner">
+        <div class="timeline-item-time">12:30</div>
+        <div class="timeline-item-title">Title</div>
+        <div class="timeline-item-subtitle">Subtitle</div>
+        <div class="timeline-item-text">Text</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'createTimeline',
+    data: () => {
+      return {
+        timeline: {
+          date: {
+            day: '',
+            month: ''
+          },
+          time: '',
+          desc: ''
+        }
+      }
+    },
+  computed: {
+    
+  }
+
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/components/ShowTimeline.vue
@@ -0,0 +1,67 @@
+<template lang="html">
+  <div id="timeline">
+    <div id="timeline-item">
+      <div id="timeline-item-date">01 <small>JAN</small></div>
+      <div id="timeline-item-divider"></div>
+      <div id="timeline-item-content">
+        <div id="timeline-item-inner">
+          <div id="timeline-item-time">12:30</div>
+          <div id="timeline-item-title">พักทานอาหารกลางวัน</div>
+          <div id="timeline-item-subtitle">ทีมงานผู้จัดกิจกรรมทุกคน</div>
+          <div id="timeline-item-text">พักทานอาหารกลาง</div>
+        </div>
+      </div>
+    </div>
+    <br><p><button id="btn" @click="addMilestones" type="button" name="button">+</button></p>
+  </div>
+</template>
+<script type="text/javascript" src="javascript/getRandomColor.js">
+
+</script>
+<script>
+// The Vue build version to load with the `import` command
+import Framework7 from 'framework7'
+import Timeline from 'framework7/dist/components/timeline/timeline.js'
+import VueTimepicker from 'vue2-timepicker'
+Framework7.use([Timeline])
+
+export default {
+  name: 'Milestone',
+  data: {
+    milestone: {
+      date: {
+        day: '',
+        month: ''
+      },
+      time: '',
+      desc: ''
+    }
+
+  },
+  methods: {
+    addMilestones: function () {
+      prompt('Press the day', this.timeline.date.day)
+      prompt('Press the month', this.timeline.date.month)
+      prompt('Press the time', this.tineline.time)
+      prompt('Press the desc', this.tineline.desc)
+    }
+  }
+}
+</script>
+
+<style lang="css" src="framework7/dist/css/framework7.css">
+</style>
+<style media="screen">
+button#btn {
+  width: 10%;
+}
+#timeline-item{
+  justify-content: center;
+}
+#timeline-item-divider{
+  background: red;
+}
+#timeline-item-divider::after{
+  background: #bbb !important;
+}
+</style>
