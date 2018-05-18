dafe3ce23fea9fae6931cabe88e0c8bb6f21779a
banned99 <minimum1112@gmail.com>
2018-05-19 06:31:43 +0700

    remove unused file

--- src/components/Milestone.vue
+++ src/components/Milestone.vue
@@ -1,83 +1,232 @@
-<template lang="html">
-  <div class="milestone-window">
-    <h1>Notable inventions, <span>1910–2000</span></h1>
-<div class="flex-parent">
-	<div class="input-flex-container">
-		<div class="input">
-			<span data-year="1910" data-info="headset"></span>
-		</div>
-		<div class="input">
-			<span data-year="1920" data-info="jungle gym"></span>
-		</div>
-		<div class="input active">
-			<span data-year="1930" data-info="chocolate chip cookie"></span>
-		</div>
-		<div class="input">
-			<span data-year="1940" data-info="Jeep"></span>
-		</div>
-		<div class="input">
-			<span data-year="1950" data-info="leaf blower"></span>
-		</div>
-		<div class="input">
-			<span data-year="1960" data-info="magnetic stripe card"></span>
-		</div>
-		<div class="input">
-			<span data-year="1970" data-info="wireless LAN"></span>
-		</div>
-		<div class="input">
-			<span data-year="1980" data-info="flash memory"></span>
-		</div>
-		<div class="input">
-			<span data-year="1990" data-info="World Wide Web"></span>
-		</div>
-		<div class="input">
-			<span data-year="2000" data-info="Google AdWords"></span>
-		</div>
-    <div class="input">
-			<span data-year="2000" data-info="Google AdWords"></span>
-		</div>
-    <div class="input">
-			<span data-year="2000" data-info="Google AdWords"></span>
-		</div>
-    <div class="input">
-			<span data-year="2000" data-info="Google AdWords"></span>
-		</div>
-	</div>
-	<div class="description-flex-container">
-		<p>And future Call of Duty players would thank them.</p>
-		<p>Because every kid should get to be Tarzan for a day.</p>
-		<p class="active">And the world rejoiced.</p>
-		<p>Because building roads is inconvenient.</p>
-		<p>Ain’t nobody got time to rake.</p>
-		<p>Because paper currency is for noobs.</p>
-		<p>Nobody likes cords. Nobody.</p>
-		<p>Brighter than glow memory.</p>
-		<p>To capitalize on an as-yet nascent market for cat photos.</p>
-		<p>Because organic search rankings take work.</p>
-	</div>
-</div>
-
-
-
-<div style="position: absolute; bottom: 40px; right: 10px; font-size: 12px">
-	<a href="https://codepen.io/cjl750/pen/XMyRoB" target="_blank">original version with slinky mobile menu</a></div>
-<div style="position: absolute; bottom: 15px; right: 10px; font-size: 12px">
-	<a href="https://codepen.io/cjl750/pen/wdVxzV" target="_blank">alternate version with custom range input</a></div>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-header">
+          <h1>Event Milestone</h1>
+        </div>
+        <div style="display:inline-block;width:100%;overflow-y:auto;">
+          <ul class="timeline timeline-horizontal">
+            <MileStoneTestItem v-for="task in sortTaskByDue" :key="task['.key']" :task="task"/>
+            <li class="timeline-item">
+              <div class="timeline-badge primary"><i class="glyphicon glyphicon-check"></i></div>
+              <div class="timeline-panel">
+                <div class="timeline-heading">
+                  <h4 class="timeline-title">Add New Task</h4>
+                </div>
+                <div class="timeline-body">
+                  <p>Mussum ipsum cacilds, vidis litro abertis. Consetis faiz elementum girarzis, nisi eros gostis.</p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import MileStoneTestItem from '../components/MileStoneTestItem'
+import { mapGetters } from 'vuex'
+
 export default {
   name: 'milestone',
-  props: {
-    // milestone: {
-    //   type: Object,
-    //   required: true
-    // }
+  computed: {
+    ...mapGetters(['getEventMilestone']),
+    sortTaskByDue () {
+      let sortedObject = {}
+      let originalKeys = Object.keys(this.getEventMilestone)
+      let originalValues = Object.values(this.getEventMilestone)
+      let sortedValue = originalValues
+      sortedValue.sort((a, b) => a.due - b.due).forEach(el => {
+        sortedObject[originalKeys[originalValues.findIndex(el)]] = el
+      })
+      return sortedObject
+    }
+  },
+  components: {
+    MileStoneTestItem
   }
 }
 </script>
 
-<style lang="sass" scope>
-  @import '../assets/css/Milestone.sass'
+<style scoped>
+@import '../assets/css/bootstrap.min.css';
+/* Timeline */
+.page-header {
+  padding-bottom: 0px;
+  margin: 0px 0 0px;
+  border-bottom: 1px solid #eee;
+}
+.timeline,
+.timeline-horizontal {
+  list-style: none;
+  padding: 20px;
+  position: relative;
+}
+.timeline:before {
+  top: 40px;
+  bottom: 0;
+  position: absolute;
+  content: " ";
+  width: 3px;
+  background-color: #eeeeee;
+  left: 50%;
+  margin-left: -1.5px;
+}
+.timeline .timeline-item {
+  margin-bottom: 20px;
+  position: relative;
+}
+.timeline .timeline-item:before,
+.timeline .timeline-item:after {
+  content: "";
+  display: table;
+}
+.timeline .timeline-item:after {
+  clear: both;
+}
+.timeline .timeline-item .timeline-badge {
+  color: #fff;
+  width: 54px;
+  height: 54px;
+  line-height: 52px;
+  font-size: 22px;
+  text-align: center;
+  position: absolute;
+  top: 18px;
+  left: 50%;
+  margin-left: -25px;
+  background-color: #333;
+  border: 3px solid #ffffff;
+  z-index: 100;
+  border-top-right-radius: 50%;
+  border-top-left-radius: 50%;
+  border-bottom-right-radius: 50%;
+  border-bottom-left-radius: 50%;
+}
+.timeline .timeline-item .timeline-badge i,
+.timeline .timeline-item .timeline-badge .fa,
+.timeline .timeline-item .timeline-badge .glyphicon {
+  top: 2px;
+  left: 0px;
+}
+.timeline .timeline-item .timeline-badge.primary {
+  background-color: #1f9eba;
+}
+.timeline .timeline-item .timeline-badge.info {
+  background-color: #5bc0de;
+}
+.timeline .timeline-item .timeline-badge.success {
+  background-color: #59ba1f;
+}
+.timeline .timeline-item .timeline-badge.warning {
+  background-color: #d1bd10;
+}
+.timeline .timeline-item .timeline-badge.danger {
+  background-color: #ba1f1f;
+}
+.timeline .timeline-item .timeline-panel {
+  position: relative;
+  width: 46%;
+  float: left;
+  right: 16px;
+  border: 1px solid #777;
+  background: #ffffff;
+  border-radius: 2px;
+  padding: 20px;
+  -webkit-box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+  box-shadow: 0 1px 6px rgba(0, 0, 0, 0.175);
+}
+.timeline .timeline-item .timeline-panel:before {
+  position: absolute;
+  top: 26px;
+  right: -16px;
+  display: inline-block;
+  border-top: 16px solid transparent;
+  border-left: 16px solid #777;
+  border-right: 0 solid #777;
+  border-bottom: 16px solid transparent;
+  content: " ";
+}
+.timeline .timeline-item .timeline-panel .timeline-title {
+  margin-top: 0;
+  color: inherit;
+}
+.timeline .timeline-item .timeline-panel .timeline-body > p,
+.timeline .timeline-item .timeline-panel .timeline-body > ul {
+  margin-bottom: 0;
+}
+.timeline .timeline-item .timeline-panel .timeline-body > p + p {
+  margin-top: 5px;
+}
+.timeline .timeline-item:last-child:nth-child(even) {
+  float: right;
+}
+.timeline .timeline-item:nth-child(even) .timeline-panel {
+  float: right;
+  left: 16px;
+}
+.timeline .timeline-item:nth-child(even) .timeline-panel:before {
+  border-left-width: 0;
+  border-right-width: 14px;
+  left: -14px;
+  right: auto;
+}
+.timeline-horizontal {
+  list-style: none;
+  position: relative;
+  padding: 20px 0px 20px 0px;
+  display: inline-block;
+}
+.timeline-horizontal:before {
+  height: 3px;
+  top: auto;
+  bottom: 26px;
+  left: 56px;
+  right: 0;
+  width: 100%;
+  margin-bottom: 20px;
+}
+.timeline-horizontal .timeline-item {
+  display: table-cell;
+  height: 280px;
+  width: 20%;
+  min-width: 320px;
+  float: none !important;
+  padding-left: 0px;
+  padding-right: 20px;
+  margin: 0 auto;
+  vertical-align: bottom;
+}
+.timeline-horizontal .timeline-item .timeline-panel {
+  top: auto;
+  bottom: 64px;
+  display: inline-block;
+  float: none !important;
+  left: 0 !important;
+  right: 0 !important;
+  width: 100%;
+  margin-bottom: 20px;
+}
+.timeline-horizontal .timeline-item .timeline-panel:before {
+  top: auto;
+  bottom: -16px;
+  left: 28px !important;
+  right: auto;
+  border-right: 16px solid transparent !important;
+  border-top: 16px solid #777 !important;
+  border-bottom: 0 solid #777 !important;
+  border-left: 16px solid transparent !important;
+}
+.timeline-horizontal .timeline-item:before,
+.timeline-horizontal .timeline-item:after {
+  display: none;
+}
+.timeline-horizontal .timeline-item .timeline-badge {
+  top: auto;
+  bottom: 0px;
+  left: 43px;
+}
 </style>
