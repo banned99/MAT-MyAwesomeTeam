5e6acbe8efd12048e02ac30ed149c6e3765966a0
wertanwee <57070057@kmitl.ac.th>
2018-05-21 11:25:59 +0700

    patch bg

--- src/App.vue
+++ src/App.vue
@@ -35,12 +35,9 @@ export default {
   src: url('assets/fonts/fontawesome-webfont.woff');
 }
 body {
-<<<<<<< HEAD
+
   background:url('assets/images/bg.jpg') no-repeat 0% 100% fixed;
-=======
-  background:url('assets/images/bg.jpg') repeat 0% 100% fixed;
->>>>>>> a10a01dca6dc755a23f1797e74cb3cf4dab2b400
-  background-size: cover;
+  background-attachment: cover;
 }
 
 #app {
